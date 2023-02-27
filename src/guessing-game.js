e08267ec6e76c20f71589e0581638ee81bf9239e
ulianava <uliana.averianova@gmail.com>
2023-02-27 16:48:34 +0200

    fea: gues game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+       return this.guessNumber = Math.round((this.max + this.min)/2)
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber
     }
 }
 
